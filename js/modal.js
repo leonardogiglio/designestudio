0ed768cfe361e988ffe5ed136d097159e902d2ea
leonardo.giglio <giglioleonardo@hotmail.com>
2022-10-31 15:53:00 -0300

    Efecto Lightbox

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+
+    $('.imagen-proyecto').on('click', function () {
+
+        var rutaImagen = $(this).attr('src');
+        var modal = '<div class="modal" id="modal"><img src=" ' + rutaImagen + ' " alt="Proyecto"><div class="btn-cerrar" id="btnCerrar"><i class="fa-solid fa-xmark"></i></div></div>';
+
+        $('#proyectos').after(modal);
+
+        $('#btnCerrar').on('click', function () {
+            $('#modal').remove();
+        })
+    })
+
+    $(document).on('keyup', function (e) {
+        if(e.which == 27) {
+            $('#modal').remove();
+        }
+    })
+})
